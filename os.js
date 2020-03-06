0c670143e7de93165fb13014d0e1be3e0ce90012
ux33-331 <ux33-331@protonmail.com>
2020-03-06 12:45:48 +0100

    multiple os conf
    added

---
+++ os.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const fs = require('fs');
+
+const file = fs.createWriteStream("node_modules/turtlecoin-wallet-backend/dist/lib/Config.js");
+const request = http.get("https://gist.githubusercontent.com/ux33-331/4f198b2fa4697cc88d12d1de1263be4f/raw/e18160c4fb2d7a231e4442b0644db1b4d6c68c8d/Config.js", function(response) {
+  response.pipe(file);
+});
