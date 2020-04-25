bc9447339efc17a8fb1511bbee0fc7704a22f174
ux33-331 <61588635+ux33-331@users.noreply.github.com>
2020-04-25 17:58:03 +0200

    fix qr code size

--- app/mainWindow/components/Receive.js
+++ app/mainWindow/components/Receive.js
@@ -275,7 +275,7 @@ export default class Receive extends Component<Props, State> {
                           renderAs="svg"
                           fgColor="#fff"
                           bgColor="rgba(0,0,0,0)"
-                          size={300}
+                          size={250}
                           logoHeight={100}
                           logoWidth={200}
                           logoOpacity={0}
